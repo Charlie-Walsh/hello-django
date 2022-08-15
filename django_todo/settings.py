44b1d8b4e9743b7aba90f2b19aaf6462623f0ec1
Charlie-Walsh <charlie.walsh.141@gmail.com>
2022-08-15 22:30:56 +0000

    key removed

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zp(@9yc2+@a(6hrvtn^jk3fy2j_bmze&6k!45g-_%7c4im1apu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
